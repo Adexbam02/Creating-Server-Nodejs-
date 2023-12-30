2274da22a7360d00a9b0af6cd032d7749b791554
Nurain <nurainbamidelejnr@gmail.com>
2023-12-30 11:30:49 +0100

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 //express app
 const app = express();
@@ -9,25 +10,38 @@ app.set("view engine", "ejs");
 //listen for requests
 app.listen(3300);
 
+// middleware & static file
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   //   res.send("<p>Home</p>");
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectur'},
-    {title: 'Mario finds eggs', snippet: 'Lorem ipsum dolor sit amet consectur'},
-    {title: 'How to finds eggs', snippet: 'Lorem ipsum dolor sit amet consectur'}
-  ]
-  res.render("index", {title: 'Home', blogs});
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+    {
+      title: "Mario finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+    {
+      title: "How to finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {title: 'About'});
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: 'Create'});
+  res.render("create", { title: "Create" });
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404", {title: 'error'});
+  res.status(404).render("404", { title: "error" });
 });
